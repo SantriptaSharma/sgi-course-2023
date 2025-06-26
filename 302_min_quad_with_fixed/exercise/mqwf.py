7cc4234bba4aee57bb98e0f77734be9b9194dcce
SantriptaSharma <santripta@gmail.com>
2025-06-26 16:58:09 +0530

    complete: module 302

--- 302_min_quad_with_fixed/exercise/mqwf.py
+++ 302_min_quad_with_fixed/exercise/mqwf.py
@@ -1,5 +1,9 @@
 import numpy as np
 import scipy
+import scipy.linalg
+import scipy.optimize
+import scipy.sparse
+import scipy.sparse.linalg
 
 def mqwf(A, B, known, known_val):
     """
@@ -19,9 +23,23 @@ def mqwf(A, B, known, known_val):
     """
 
     #### Fill in the missing part #####
+    unknown = np.setdiff1d(np.arange(A.shape[0]), known)
+    An = A[unknown, :][:, unknown]
 
+    bn = A[unknown, :][:, known] * known_val + (A[known, :][:, unknown]).T * known_val + B[unknown]
+
+    # adding eq constraints just requires tacking on the Aeq and beq rows
+    # [ 2*An ] x = [ -bn ]
+    # [ Aeq  ]     [ beq ]
+
+    unknowns = scipy.sparse.linalg.spsolve(2 * An, -bn)
 
     ###################################
 
+    dim = known_val.shape[-1]
+
+    u = np.zeros((A.shape[0], dim))
+    u[known] = known_val
+    u[unknown] = unknowns
 
     return u
